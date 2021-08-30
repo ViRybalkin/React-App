b72e986e8cf61b7ef7ad47ab8f2f3be85591e72e
Vitaliy <kinjeks@gmail.com>
2021-08-30 17:12:47 +0300

    add expens components

---
+++ src/components/Expenses.js
@@ -0,0 +1,25 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+export default function Expenses(props) {
+  console.log(props);
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.item[0].title}
+        amount={props.item[0].amount}
+        date={props.item[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.item[1].title}
+        amount={props.item[1].amount}
+        date={props.item[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.item[2].title}
+        amount={props.item[2].amount}
+        date={props.item[2].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
