614876fe74a78260100f7bd0d02a504daee1e91e
Vitaliy <kinjeks@gmail.com>
2021-09-01 13:33:44 +0300

    Add functionality of filtering items by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,15 +6,21 @@ import React, { useState } from 'react';
 
 export const Expenses = props => {
   const [filteredYear, setFilteredYear] = useState('2022');
+
   const changeFilter = year => {
     setFilteredYear(year);
   };
 
+  const filteredExpenses = props.item.filter(item => {
+    return item.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpenseFilter selected={filteredYear} onChangeFilter={changeFilter} />
-      {props.item.map(expense => (
+      {filteredExpenses.map(expense => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
